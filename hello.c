70b4e407d15773c46be164dacd03ae1103813af4
PrinciplesOfDevelopment <78975685+PrinciplesOfDevelopment@users.noreply.github.com>
2021-06-08 08:51:33 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: John Kunkee/Xuan Wu
+ *         cbourke@cse.unl.edu
+ * Date: 2021/05/07
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("John Kunkee/Xuan Wu\n");
+
+  return 0;
+}
